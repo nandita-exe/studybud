0021cc0eec8cefc6ae2ec1aeb553ab5bd15a65fd
nandita-exe <85404089+nandita-exe@users.noreply.github.com>
2022-04-09 10:05:48 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
 STATICFILES_DIRS = [
